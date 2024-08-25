bce88c65ce5c1e0d3ce295faa2721099f90e45bc
srinathd12 <srinathdevan12@gmail.com>
2024-08-25 17:12:10 +0530

    add slider

--- supports.php
+++ supports.php
@@ -4,26 +4,22 @@
 
 <div class="supt_float_container">
     <div id="top_float_div">
-        <span>Call us</span>
-
-        <div class="call_float_btn">
-            <a href="tel:+919488434411" ><i class="fa-solid fa-phone"></i></a>
-        </div>
+        <a class="call_float_btn mb-3" href="tel:+919488434411"><i class="fa-solid fa-phone"></i></a>
     </div>
 
     <div id="top_float_div">
-        <span>Chat us</span>
-
-        <div class="whatsapp_float_btn">
-            <a href="#" ><i class="fa-brands fa-whatsapp"></i></a>
-        </div>
+        
+        <a class="whatsapp_float_btn" target="blank" href="https://wa.me/9488434411"><i class="fa-brands fa-whatsapp"></i></a>
     </div>
-    <button class="float_btn"><i class="fa-solid fa-headset"></i></button>
+
+    <div>
+        <button class="float_btn"><i class="fa-solid fa-headset"></i></button>
+    </div>
 </div>
 
 
 
-<div class="supt_float_container_mob" style="display:none;">
-    <a href="#" class=""><i class="fa-solid fa-phone"></i>Call us</a>
-    <a href="#" class=""><i class="fa-brands fa-whatsapp"></i>Chat us</a>
+<div class="supt_float_container_mob text-center" style="">
+    <a href="tel:+919488434411" class=""><i class="fa-solid fa-phone"></i>Call us</a>
+    <a href="https://wa.me/9488434411" target="blank" class=""><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
 </div>
