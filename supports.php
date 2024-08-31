d288fcb5d58e3035dddf8aa0f8095052448a24af
srinathd12 <srinathdevan12@gmail.com>
2024-08-31 17:01:19 +0530

    make some changes

--- supports.php
+++ supports.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 
-=======
 <?php include 'links.php'; ?>
->>>>>>> 1c99d0ef98ccbe1f9a3b2ad80ed8c640df1a2086
 <link rel="stylesheet" href="css/support.v.2.css">
 <!--  Supports Float Button -->
 
