681a8fdedf83f53a50db0cfe60a54145d1830efd
srinathd12 <srinathdevan12@gmail.com>
2024-10-18 16:54:28 +0530

    Added a new page

--- std_doubts.php
+++ std_doubts.php
@@ -1,4 +1,8 @@
 <style>
+    .card-header a {
+        font-size: 15px !important;
+    }
+
     #accordion>div {
         margin-bottom: 15px;
     }
@@ -9,16 +13,19 @@
         display: inline-block;
         padding: 7px;
         text-decoration: none;
-        font-size: 20px;
         color: #323130 !important;
+    }
+
+    .card-body{
+        font-size: 15px !important;
     }
 
     #accordion a i {
         position: absolute;
-        right: 20px;
-        top: 15px;
+        right: 10px;
+        top: 13px;
         color: #6e6e6e !important;
-        font-size: 15px;
+        font-size: 10px;
     }
 </style>
 <!-- Students Doubts  -->
@@ -46,26 +53,28 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseTwo">
-                Who can join your training programs?<i class="fa-solid fa-chevron-down"></i>
+                    Who can join your training programs?<i class="fa-solid fa-chevron-down"></i>
                 </a>
             </div>
             <div id="collapseTwo" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                Anyone interested in learning technology can join. We offer courses for students, working professionals, and even
-                school kids from 6th to 12th grade.
+                    Anyone interested in learning technology can join. We offer courses for students, working
+                    professionals, and even
+                    school kids from 6th to 12th grade.
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseThree">
-                Do you provide job placement assistance after training?<i class="fa-solid fa-chevron-down"></i>
+                    Do you provide job placement assistance after training?<i class="fa-solid fa-chevron-down"></i>
                 </a>
             </div>
             <div id="collapseThree" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                Yes, we help students with job placement after they complete the course. Many of our students have been placed in
-                top companies.
+                    Yes, we help students with job placement after they complete the course. Many of our students have
+                    been placed in
+                    top companies.
                 </div>
             </div>
         </div>
@@ -73,13 +82,14 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseFour">
-                What is the fee structure for your courses?<i class="fa-solid fa-chevron-down"></i>
+                    What is the fee structure for your courses?<i class="fa-solid fa-chevron-down"></i>
                 </a>
             </div>
             <div id="collapseFour" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                Our fees vary depending on the course. You can contact us or visit our website for the latest fee details. We aim to
-                offer quality training at affordable prices.
+                    Our fees vary depending on the course. You can contact us or visit our website for the latest fee
+                    details. We aim to
+                    offer quality training at affordable prices.
                 </div>
             </div>
         </div>
@@ -87,13 +97,14 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseFive">
-                Is the training practical or theoretical?<i class="fa-solid fa-chevron-down"></i>
+                    Is the training practical or theoretical?<i class="fa-solid fa-chevron-down"></i>
                 </a>
             </div>
             <div id="collapseFive" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                Our training is mostly hands-on. We focus on practical learning so you can apply the skills directly in real-world
-                projects.
+                    Our training is mostly hands-on. We focus on practical learning so you can apply the skills directly
+                    in real-world
+                    projects.
                 </div>
             </div>
         </div>
@@ -101,14 +112,17 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseSix">
-                Where is your office located, and how do I contact you?<i class="fa-solid fa-chevron-down"></i>
+                    Where is your office located, and how do I contact you?<i class="fa-solid fa-chevron-down"></i>
                 </a>
             </div>
             <div id="collapseSix" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                We are located in Hosur<a style="text-decoration:underline; display:inline; color:#004bee !important;" href="https://www.google.com/maps/place/Arka+Networkz+System/@12.7360388,77.8222739,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae7185e99a9cb5:0x3520348054405036!8m2!3d12.7360336!4d77.8248488!16s%2Fg%2F11t1ds8dv0?authuser=0&entry=ttu"
-                target="_blank">click</a>here. You can visit our office, call us, or reach out via our website for more details. We
-are happy to help!
+                    We are located in Hosur<a
+                        style="text-decoration:underline; display:inline; color:#004bee !important;"
+                        href="https://www.google.com/maps/place/Arka+Networkz+System/@12.7360388,77.8222739,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae7185e99a9cb5:0x3520348054405036!8m2!3d12.7360336!4d77.8248488!16s%2Fg%2F11t1ds8dv0?authuser=0&entry=ttu"
+                        target="_blank">click</a>here. You can visit our office, call us, or reach out via our website
+                    for more details. We
+                    are happy to help!
                 </div>
             </div>
         </div>
