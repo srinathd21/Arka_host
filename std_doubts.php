80771cc0dca4e40aec84f31f2fe9526ced47ff1d
srinathd12 <srinathdevan12@gmail.com>
2024-11-02 17:13:12 +0530

    added mobile search

--- std_doubts.php
+++ std_doubts.php
@@ -5,17 +5,19 @@
         font-style: normal;
         color: rgb(52, 52, 52);
     }
-    .card{
+
+    .card {
         width: 60vw;
         border: none;
         border-left: 3px solid #23bca0;
-        background-color: rgb(238, 254, 249,0.1);
+        background-color: rgb(238, 254, 249, 0.1);
         border-radius: 10px;
         color: rgb(52, 52, 52);
 
     }
-    .card-header{
-        border-radius: 10px !important,0px;
+
+    .card-header {
+        border-radius: 10px !important, 0px;
     }
 
     #accordion>div {
@@ -31,7 +33,7 @@
         color: #323130 !important;
     }
 
-    .card-body{
+    .card-body {
         font-size: 15px !important;
     }
 
@@ -43,18 +45,20 @@
         font-size: 13px;
         font-weight: 600;
     }
-.rotate-icon.active {
-    transform: rotate(180deg);
-}
 
-    @media screen and (max-width: 992px){
-        .card{
+    .rotate-icon.active {
+        transform: rotate(180deg);
+    }
+
+    @media screen and (max-width: 992px) {
+        .card {
             width: 100%;
         }
+
         #accordion a i {
 
-        right: -10px;
-    }
+            right: -10px;
+        }
     }
 </style>
 <!-- Students Doubts  -->
@@ -71,11 +75,11 @@
             </div>
             <div id="collapseOne" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    We offer IT training, internships, inplant training, and software development services. You can
-                    learn advance
-                    technologies in programming, cloud computing, and more. We also help businesses with website
-                    development, app
-                    customization, and IT solutions.
+                    Arka technologies provide IT trainings, internships, inplant trainings, and software development
+                    services. You can
+                    learn advance technologies in programming, cloud computing, and more. And also help Micro and Small
+                    businesses
+                    with website development, app customization, and IT solutions.
                 </div>
             </div>
         </div>
@@ -87,23 +91,24 @@
             </div>
             <div id="collapseTwo" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    Anyone interested in learning technology can join. We offer courses for students, working
-                    professionals, and even
-                    school kids from 6th to 12th grade.
+                    Anyone interested in learning technology can join. Arka specifically offers courses for students,
+                    working professionals,
+                    and school students from 6th to 12th grade.
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseThree">
-                    Do you provide job placement assistance after training?<i class="fa-solid fa-chevron-down rotate-icon"></i>
+                    Does arka technology provides job placement assistance after training?<i
+                        class="fa-solid fa-chevron-down rotate-icon"></i>
                 </a>
             </div>
             <div id="collapseThree" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    Yes, we help students with job placement after they complete the course. Many of our students have
-                    been placed in
-                    top companies.
+                    Yes, arka technologies offers students those who completed a prior course with job placement after
+                    the completion
+                    of the course.
                 </div>
             </div>
         </div>
@@ -111,14 +116,15 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseFour">
-                    What is the fee structure for your courses?<i class="fa-solid fa-chevron-down rotate-icon"></i>
+                    What is the fee structure for courses in arka technologies?<i
+                        class="fa-solid fa-chevron-down rotate-icon"></i>
                 </a>
             </div>
             <div id="collapseFour" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    Our fees vary depending on the course. You can contact us or visit our website for the latest fee
-                    details. We aim to
-                    offer quality training at affordable prices.
+                    Fee’s structure varies depending on the course. You can contact us or visit arka technologies
+                    official website for the
+                    latest fee details. Arka technologies aim to offer quality training at affordable prices.
                 </div>
             </div>
         </div>
@@ -131,9 +137,9 @@
             </div>
             <div id="collapseFive" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    Our training is mostly hands-on. We focus on practical learning so you can apply the skills directly
-                    in real-world
-                    projects.
+                    Training in arka technologies is mostly hands-on. And focusing on practical learning so you can
+                    apply the skills directly
+                    in real-world projects.
                 </div>
             </div>
         </div>
@@ -141,35 +147,61 @@
         <div class="card">
             <div class="card-header">
                 <a class="collapsed" data-bs-toggle="collapse" href="#collapseSix">
-                    Where is your office located, and how do I contact you?<i class="fa-solid fa-chevron-down rotate-icon"></i>
+                    Where is Arka Technologies Located?<i class="fa-solid fa-chevron-down rotate-icon"></i>
                 </a>
             </div>
             <div id="collapseSix" class="collapse" data-bs-parent="#accordion">
                 <div class="card-body">
-                    We are located in Hosur<a
-                        style="text-decoration:underline; display:inline; color:#004bee !important;"
-                        href="https://www.google.com/maps/place/Arka+Networkz+System/@12.7360388,77.8222739,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae7185e99a9cb5:0x3520348054405036!8m2!3d12.7360336!4d77.8248488!16s%2Fg%2F11t1ds8dv0?authuser=0&entry=ttu"
-                        target="_blank">click</a>here. You can visit our office, call us, or reach out via our website
-                    for more details. We
-                    are happy to help!
+                    Arka Technologies located in the urban area of Hosur. Our office is just 100-meter walk from the
+                    Hosur Main Bus Stand
+                    and 1.2 KMs from the Hosur Railway Station.<br><br>
+                    How to Contact Us:<br><br>
+                    No prior appointment is required, you can visit us directly!
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <a class="collapsed" data-bs-toggle="collapse" href="#collapseSeven">
+                    Why Choose Arka Technologies for Your Career Growth?<i
+                        class="fa-solid fa-chevron-down rotate-icon"></i>
+                </a>
+            </div>
+            <div id="collapseSeven" class="collapse" data-bs-parent="#accordion">
+                <div class="card-body">
+                    Arka Technologies as a premier training institute in hosur offers cutting-edge courses that align
+                    with the latest industry
+                    trends and technological advancements, ensuring students are always ahead of the curve. From
+                    foundational training
+                    to advanced skill development, Arka Technologies has been trusted by many, as reflected in our
+                    positive Google reviews
+                    from satisfied students.
+                    <br><br>
+                    Check out what our clients have to say: <a style="text-decoration:underline; display:inline; color:#004bee !important;"
+    href="https://www.google.com/maps/place/Arka+Networkz+System/@12.7360388,77.8222739,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae7185e99a9cb5:0x3520348054405036!8m2!3d12.7360336!4d77.8248488!16s%2Fg%2F11t1ds8dv0?authuser=0&entry=ttu"
+    target="_blank">Click here [https://maps.app.goo.gl/7kcv46nm5zt3m3DGA?g_st=iwb]</a>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <script>
     document.querySelectorAll('[data-bs-toggle="collapse"]').forEach(function (element) {
-    element.addEventListener('click', function () {
-        let icon = this.querySelector('.rotate-icon');
-        let targetCollapse = document.querySelector(this.getAttribute('href'));
+        element.addEventListener('click', function () {
+            let icon = this.querySelector('.rotate-icon');
+            let targetCollapse = document.querySelector(this.getAttribute('href'));
 
-        targetCollapse.addEventListener('shown.bs.collapse', function () {
-            icon.classList.add('active');
-        });
-        targetCollapse.addEventListener('hidden.bs.collapse', function () {
-            icon.classList.remove('active');
+            targetCollapse.addEventListener('shown.bs.collapse', function () {
+                icon.classList.add('active');
+            });
+            targetCollapse.addEventListener('hidden.bs.collapse', function () {
+                icon.classList.remove('active');
+            });
         });
     });
-});
 
 </script>
