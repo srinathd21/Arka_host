9907a15f9dfce4c25bd33461bb11b9d0860c920f
srinathd12 <srinathdevan12@gmail.com>
2024-11-16 15:21:56 +0530

    update customer pages

--- links.php
+++ links.php
@@ -1,6 +1,4 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Guides&display=swap" rel="stylesheet">
+
 <!-- Bootstrap -->
 <link href="bootstrap-5.3.0-dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="bootstrap-5.3.0-dist/js/bootstrap.bundle.min.js"></script>
